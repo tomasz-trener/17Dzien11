acd48824e63cf2de8963de5a10baf100e4c0bf67
kurs <kurs@DESKTOP-V3HDKPQ>
2022-12-19 09:56:25 +0100

    Add project files.

---
+++ P01CoreWstep/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Net;
+using System.Text.RegularExpressions;
+
+while (true)
+{
+    
+
+    Console.WriteLine("Podaj nazwe miasta");
+    string miasto = Console.ReadLine();
+    string adres = $"https://www.google.com/search?q=pogoda {miasto}";
+
+    WebClient wc = new WebClient();
+    string dane = wc.DownloadString(adres);
+
+    //File.WriteAllText("strona.html", dane);
+
+    string szablon = "<div class=\"BNeawe iBp4i AP7Wnd\">(-{0,1}\\d{1,3}).C<\\/div>";
+    Regex rx = new Regex(szablon);
+    Match match = rx.Match(dane);
+
+    // string wynik = match.Groups[0].Value;
+    string wynik = match.Groups[1].Value;
+
+    Console.WriteLine(wynik);
+}
